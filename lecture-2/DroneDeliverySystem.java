e3ef4b888e181eea23437e9956c4864503106863
Aditya Bhatia <bhatia4334@gmail.com>
2026-06-10 22:52:03 +0530

    second lecture

---
+++ lecture-2/DroneDeliverySystem.java
@@ -0,0 +1,91 @@
+abstract class DeliveryDrone{
+    protected String droneId;
+
+    public DeliveryDrone(String droneId){
+        this.droneId= droneId;
+    }
+
+    public abstract void deliverPackage();
+}
+
+interface Airborne{
+    void flyToDestination();
+
+    default void requestAirTrafficClearance(){
+        System.out.println("Air Traffic Clearance Granted.");
+    }
+}
+
+interface GroundBased{
+    void navigateSidewalks();
+}
+
+class Quadcopter extends DeliveryDrone implements Airborne{
+    public Quadcopter(String droneId){
+        super(droneId);
+    }
+
+    @Override
+    public void flyToDestination(){
+        System.out.println("Quadcopter" + droneId+ " flying to destinationj.");
+    }
+
+    @Override
+    public void deliverPackage(){
+        requestAirTrafficClearance();
+        flyToDestination();
+        System.out.println("Package delivered by Quadcopter.\n");
+    }
+}
+
+class CityRover extends DeliveryDrone implements GroundBased{
+    public CityRover(String droneId){
+        super(droneId);
+    }
+
+    @Override
+    public void navigateSidewalks(){
+        System.out.println("CityRover "+ droneId+ "navigating sidewalks.");
+    }
+
+    @Override
+    public void deliverPackage(){
+        navigateSidewalks();
+        System.out.println("package delivered by City Rover.\n");
+    }
+}
+
+class HybridVTOL extends DeliveryDrone implements Airborne, GroundBased{
+    public HybridVTOL(String droneId){
+        super(droneId);
+    }
+
+    @Override
+    public void flyToDestination(){
+        System.out.println("HybridVTOL " + droneId+ " flying to Destination.");
+    }
+
+    @Override
+    public void navigateSidewalks(){
+        System.out.println("HybridVTOL "+ droneId+ " navigating on ground.");
+    }
+
+    @Override
+    public void deliverPackage(){
+        requestAirTrafficClearance();
+        flyToDestination();
+        navigateSidewalks();
+        System.out.println("Package delivered by HybridVTOL.\n");
+    }
+}
+public class DroneDeliverySystem {
+    public static void main(String[] args){
+        DeliveryDrone d1 = new Quadcopter("Q101");
+        DeliveryDrone d2 = new CityRover("C202");
+        DeliveryDrone d3= new HybridVTOL("H303");
+
+        d1.deliverPackage();
+        d2.deliverPackage();
+        d3.deliverPackage();
+    }
+}
